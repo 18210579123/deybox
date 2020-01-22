5948e9fc24690b07dab7dd73894c36c2c6e46d52
18201579123 <a@a.com>
2020-01-23 01:29:34 +0800

    版本号父pom变量，加junit

--- deybox-spring-simple/src/main/java/com/dey/deybox/spring/simple/ServiceCaller.java
+++ deybox-spring-simple/src/main/java/com/dey/deybox/spring/simple/ServiceCaller.java
@@ -8,17 +8,21 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * 当然，需要传入配置类参数
  */
 public class ServiceCaller {
-    public static void main(String[] args) {
-        /**
-         * 这个地方，如果没有为AnnotationConfigApplicationContext配置参数，会报错：
-         * Exception in thread "main" java.lang.IllegalStateException:
-         * org.springframework.context.annotation.AnnotationConfigApplicationContext@21b8d17c
-         * has not been refreshed yet
-         * 只有设置了参数，才会refresh，通过初始化的状态
-         */
+
+    /**
+     * 这个地方，如果没有为AnnotationConfigApplicationContext配置参数，会报错：
+     * Exception in thread "main" java.lang.IllegalStateException:
+     * org.springframework.context.annotation.AnnotationConfigApplicationContext@21b8d17c
+     * has not been refreshed yet
+     * 只有设置了参数，才会refresh，通过初始化的状态
+     */
+    public void call(){
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DeyConfig.class);
         GreetingService greetingService = applicationContext.getBean(GreetingService.class);
         greetingService.greeting();
+    }
 
+    public static void main(String[] args) {
+        new ServiceCaller().call();
     }
 }
---
+++ deybox-spring-simple/src/test/java/com/dey/deybox/spring/simple/ServiceCallerTest.java
@@ -0,0 +1,13 @@
+package com.dey.deybox.spring.simple;
+
+
+import org.junit.Test;
+
+
+public class ServiceCallerTest {
+
+    @Test
+    public void call(){
+        new ServiceCaller().call();
+    }
+}
